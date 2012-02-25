7ce76a824d7b9e85540aac61a09f39799955b06c
Grégoire HUBERT <gregoire.hubert.pro@gmail.com>
2012-02-26 00:17:14 +0100

    Transaction

--- bin/get_weather_report.php
+++ bin/get_weather_report.php
@@ -12,18 +12,30 @@ $city_map = $connection
 $probe_xml_map = $connection
     ->getMapFor("Greg\Weather\ProbeXml");
 
+$weather_probe_map = $connection
+    ->getMapFor("Greg\Weather\WeatherProbe");
+
 $cities = $city_map->findAll();
 
 foreach($cities as $city) {
     try {
-        $probe_xml = $probe_xml_map
-          ->createObject(array(
+        $connection->begin();
+        $probe_xml = $probe_xml_map->createObject(array(
             'city_name' => $city->get('name'),
-            'xml_response' => utf8_encode(file_get_contents(sprintf("http://www.google.com/ig/api?weather=%s&hl=fr", urlencode($city->get('name')))))
+            'xml_response' => utf8_encode(
+              file_get_contents(
+                sprintf("http://www.google.com/ig/api?weather=%s&hl=fr", urlencode($city->get('name')))))
         ));
         $probe_xml_map->saveOne($probe_xml);
+        $city->setWeatherProbe(
+          $weather_probe_map
+            ->findWhere('city_name = ?', array($city->get('name')),
+              'ORDER BY created_at DESC LIMIT 1')->current());
+        $city_map->updateOne($city, array('weather_probe'));
+        $connection->commit();
         sleep(1);
     } catch(\Pomm\Exception\Exception $e) {
+        $connection->rollback();
         printf("Error wile processing city '%s'.\n Driver said '%s'.\nIgnoring.\n", $city['name'], $e->getMessage());
     }
 }
